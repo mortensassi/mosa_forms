cfdc7427c978e97cd332886e0c8e17fcfdf3b709
Morten Sassi <dev@mortensassi.com>
2021-12-15 20:23:50 +0100

    add german country list finish search select

--- public/app/src/components/FormCountries.vue
+++ public/app/src/components/FormCountries.vue
@@ -1,6 +1,7 @@
 <script>
-import { computed } from 'vue'
-import countriesList from "@/assets/countries.js";
+import Fuse from 'fuse.js'
+import {ref, computed, watch} from 'vue'
+import countriesList from "@/assets/countries.json";
 
 export default {
   name: 'FormCountryselect',
@@ -15,13 +16,18 @@ export default {
     }
   },
 
-  setup() {
+  setup(props) {
+    const searchInput = ref('')
+    const autocompleteStr = ref('')
+    const selection = ref(null)
+    const showDropdown = ref(false)
+
     const countries = computed(() => {
       return countriesList.reduce((r, e) => {
         // get first letter of name of current element
         let group = e.name[0];
         // if there is no property in accumulator with this letter create it
-        if(!r[group]) r[group] = {group, children: [e]}
+        if (!r[group]) r[group] = {group, children: [e]}
         // if there is push current element to children array for that letter
         else r[group].children.push(e);
         // return accumulator
@@ -29,7 +35,57 @@ export default {
       }, {})
     })
 
-    return { countries }
+    const searchEngine = () => {
+      const options = {keys: ['name']}
+      const index = Fuse.createIndex(options.keys, countriesList)
+
+      return new Fuse(countriesList, options, index)
+    }
+
+    const searchRequest = (val) => {
+      selection.value = ''
+      autocompleteStr.value = ''
+
+      const result = searchEngine().search(val)
+      if (result[0]) {
+        const resultStr = result[0].item.name
+        autocompleteStr.value = val + resultStr.slice(val.length)
+      }
+    }
+
+    const setSelection = (val) => {
+      if (typeof val === 'string') {
+        selection.value = val
+        searchInput.value = val
+        autocompleteStr.value = ''
+      }
+      else if (autocompleteStr.value) {
+        selection.value = autocompleteStr.value
+        searchInput.value = autocompleteStr.value
+      }
+    }
+
+    const toggleDropdown = (val) => {
+      showDropdown.value = val
+    }
+
+    watch(selection, (n) => {
+      if (n) {
+        document.getElementById(`Countries-${props.index}-input`).blur()
+        toggleDropdown(false)
+      }
+    })
+
+    return {
+      countries,
+      searchInput,
+      searchRequest,
+      autocompleteStr,
+      setSelection,
+      selection,
+      toggleDropdown,
+      showDropdown
+    }
   }
 }
 </script>
@@ -51,24 +107,39 @@ export default {
             />
             <input
               :id="`Countries-${index}-input`"
+              v-model="searchInput"
               type="text"
-              class="c-input__control"
+              class="c-input__control msf-select__search-input"
+              @input="searchRequest(searchInput)"
+              @keydown.enter="setSelection"
+              @focus="toggleDropdown(true)"
+              @click="setSelection(true)"
             >
+            <span
+              v-if="autocompleteStr"
+              class="c-input__control msf-select__autocomplete"
+            >{{ autocompleteStr }}</span>
           </div>
-          <div
-            v-for="(country, countryIndex) in countries"
-            :key="`Countries-${index}-group-${countryIndex}`"
-            class="msf-select__group"
-          >
-            <span class="msf-select__group-label">{{ country.group }}</span>
-            <button
-              v-for="(child, childIndex) in country.children"
-              :key="`Countries-${index}-group-${countryIndex}-child-${childIndex}`"
-              class="msf-select__choice"
-            >
-              {{ child.name }}
-            </button>
-          </div>
+          <transition name="moveup">
+            <div class="msf-select__group-wrapper" v-if="showDropdown">
+              <div
+                v-for="(country, countryIndex) in countries"
+                :key="`Countries-${index}-group-${countryIndex}`"
+                class="msf-select__group"
+              >
+                <span class="msf-select__group-label">{{ country.group }}</span>
+                <button
+                  v-for="(child, childIndex) in country.children"
+                  :key="`Countries-${index}-group-${countryIndex}-child-${childIndex}`"
+                  :class="{'is-selected' : child.name === selection}"
+                  class="msf-select__choice"
+                  @click="setSelection(child.name)"
+                >
+                  {{ child.name }}
+                </button>
+              </div>
+            </div>
+          </transition>
         </div>
       </div>
     </div>
